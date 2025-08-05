a0dec122dab2ee7c19421aca6ac18c8bbdaf6e45
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-08-05 16:15:23 -0600

    wallpaper shenanigans

---
+++ dots/.config/hypr/scripts/random-wall.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+WALL_DIR=/home/jaq/dots/dots/.walls/
+CURRENT_WALL=/home/jaq/dots/dots/.walls/current.png
+
+cp $(find $WALL_DIR -type f -print0 | shuf -z -n 1 | tr -d '\0') $CURRENT_WALL
+
+swww img $CURRENT_WALL --transition-type any --transition-duration 0.5
