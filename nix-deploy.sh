719b973a070f8c1046c9070893ed4c29e737c15f
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-08-12 14:57:49 -0600

    fix nix-deploy script

--- nix-deploy.sh
+++ nix-deploy.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-NIX_CONFIG="/home/jaq/dots/dots/nix/configuration.nix"
-HARDWARE_NIX_CONFIG="/home/jaq/dots/dots/nix/hardware-configuration.nix"
+OG_NIX_CONFIG="/etc/nixos/configuration.nix"
+OG_HARDWARE_CONFIG="/etc/nixos/hardware-configuration.nix"
 
-if [ ! -f "$NIX_CONFIG" ]; then
-    mv /etc/nixos/configuration.nix "$NIX_CONFIG"
+DOTS_NIX_CONFIG="/home/jaq/dots/dots/nix/configuration.nix"
+DOTS_HARDWARE_CONFIG="/home/jaq/dots/dots/nix/hardware-configuration.nix"
+
+if [ ! -f "$DOTS_HARDWARE_CONFIG" ]; then
+    echo "$DOTS_HARDWARE_CONFIG does not exist."
+    mv "$OG_HARDWARE_CONFIG" "$DOTS_HARDWARE_CONFIG"
+    echo "Moved $OG_HARDWARE_CONFIG to $DOTS_HARDWARE_CONFIG."
 fi
 
-if [ ! -f "$HARDWARE_NIX_CONFIG" ]; then
-    mv /etc/nixos/hardware-configuration.nix "$HARDWARE_NIX_CONFIG"
+if [ -e "$OG_NIX_CONFIG" ]; then
+    echo "Deleting $OG_NIX_CONFIG."
+    sudo rm -f "$OG_NIX_CONFIG"
+
+    read -p "Do you want to rebuild the system from $DOTS_NIX_CONFIG? (y/N)" REBUILD
+
+    if [ "$REBUILD" = "" ] || [ "$REBUILD" = "N" ] || [ "$REBUILD" = "n" ]; then
+        echo "You must do so manually by running:"
+        echo "  $ sudo nixos-rebuild switch --file $DOTS_NIX_CONFIG"
+    elif [ "$REBUILD" = "y" ] || [ "$REBUILD" = "Y" ]; then
+        echo "Executing nixos-rebuild switch."
+        sudo nixos-rebuild switch --file "$DOTS_NIX_CONFIG"
+    fi
 fi
