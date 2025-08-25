dc4701443f80e3a1e2ba4e894a3763a7932bdf73
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-08-24 21:39:44 -0600

    misc fixes

--- dotfiles/hypr/scripts/random-wall.sh
+++ dotfiles/hypr/scripts/random-wall.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-WALL_DIR=/home/jaq/dots/wallpapers/
-
-CURRENT_WALL=/home/jaq/dots/wallpapers/.current.png
-
+WALL_DIR=/home/jaq/wallpapers/dots
 RANDOM_WALL=$(find $WALL_DIR -type f -print0 | shuf -z -n 1 | tr -d '\0')
 
-cp "$RANDOM_WALL" "$CURRENT_WALL"
+ln -sf $RANDOM_WALL ~/.cache/.current-wall
 
-swww img "$CURRENT_WALL" --transition-type any --transition-duration 0.4 --transition-fps 120
+swww img "$RANDOM_WALL" --transition-type any --transition-duration 0.5 --transition-fps 120
