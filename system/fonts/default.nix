95f8e9bee266140bd9913bba1eb2dbce90a2bede
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-08-26 15:53:51 -0600

    fix: simplify font config to essentials

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -4,24 +4,18 @@
       builtins.attrValues {
         inherit
           (pkgs)
-          material-icons
-          material-design-icons
           noto-fonts
-          noto-fonts-cjk-sans
-          noto-fonts-emoji
           ;
       }
       ++ [
         pkgs.nerd-fonts.jetbrains-mono
-        pkgs.nerd-fonts.meslo-lg
       ];
 
     fontconfig = {
       defaultFonts = {
-        emoji = ["Noto Color Emoji"];
         monospace = ["JetBrainsMono Nerd Font"];
-        sansSerif = ["Noto SansSerif" "Noto Color Emoji"];
-        serif = ["Noto Serif" "Noto Color Emoji"];
+        sansSerif = ["Noto SansSerif"];
+        serif = ["Noto Serif"];
       };
     };
   };
