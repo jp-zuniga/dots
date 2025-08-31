f8f70f423937ad8571ebcc2c49d8fb59cfa1682b
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-08-30 22:20:17 -0600

    fix: remove rofi background transparency

--- user/wrapped/rofi/rofi-conf.nix
+++ user/wrapped/rofi/rofi-conf.nix
@@ -6,124 +6,124 @@ pkgs.writeText "config.rasi" ''
   @import "default"
 
   configuration {
-      display-drun:        "";
-      disable-history:     true;
-      drun-display-format: "– {name}";
-      no-levenshtein-sort: true;
-      show-icons:          false;
+    display-drun: "";
+    disable-history: true;
+    drun-display-format: "– {name}";
+    no-levenshtein-sort: true;
+    show-icons: false;
   }
 
   * {
-      font:    "mono 16";
-      margin:  0px;
-      padding: 0px;
-      spacing: 0px;
+    font: "mono 16";
+    margin: 0px;
+    padding: 0px;
+    spacing: 0px;
 
-      background: rgba(35, 33, 54, 0.9);
-      foreground: ${theme.text};
+    background: ${theme.bg};
+    foreground: ${theme.text};
 
-      bordercolor: ${theme.rose};
-      textcolor:   ${theme.text};
+    bordercolor: ${theme.rose};
+    textcolor: ${theme.text};
 
-      active-background: ${theme.pine};
-      urgent-background: ${theme.love};
+    active-background: ${theme.pine};
+    urgent-background: ${theme.love};
 
-      selected-background:        ${theme.muted};
-      selected-active-background: @active-background;
-      selected-urgent-background: @urgent-background;
+    selected-background: ${theme.muted};
+    selected-active-background: @active-background;
+    selected-urgent-background: @urgent-background;
   }
 
   #window {
-      anchor:        center;
-      border:        3px;
-      border-color:  ${theme.muted};
-      border-radius: 0px;
-      enabled:       true;
-      fullscreen:    false;
-      height:        480px;
-      location:      center;
-      padding:       20px;
-      width:         350px;
+    anchor: center;
+    border: 3px;
+    border-color: ${theme.muted};
+    border-radius: 0px;
+    enabled: true;
+    fullscreen: false;
+    height: 480px;
+    location: center;
+    padding: 20px;
+    width: 350px;
   }
 
   #mainbox {
-      spacing: 10px;
+    spacing: 10px;
   }
 
   #inputbar {
-      border:        2px;
-      border-color:  @bordercolor;
-      border-radius: 5px;
-      children:      [ textbox-prompt-colon, entry ];
-      padding:       5px;
-      spacing:       0px;
+    border: 2px;
+    border-color: @bordercolor;
+    border-radius: 5px;
+    children: [ textbox-prompt-colon, entry ];
+    padding: 5px;
+    spacing: 0px;
   }
 
   #textbox-prompt-colon {
-      str:        ">";
-      text-color: ${theme.rose};
-      padding:    0px 5px 0px 0px;
+    str:        ">";
+    text-color: ${theme.rose};
+    padding:    0px 5px 0px 0px;
   }
 
   #entry {
-      padding:           0px;
-      placeholder:       "Search...";
-      placeholder-color: ${theme.muted};
-      spacing:           0px;
-      text-color:        ${theme.rose};
+    padding: 0px;
+    placeholder: "Search...";
+    placeholder-color: ${theme.muted};
+    spacing: 0px;
+    text-color: ${theme.rose};
   }
 
   #listview {
-      border:        2px;
-      border-color:  ${theme.iris};
-      border-radius: 5px;
-      scrollbar:     false;
-      padding:       0px;
-      spacing:       0px;
+    border: 2px;
+    border-color: ${theme.iris};
+    border-radius: 5px;
+    scrollbar: false;
+    padding: 0px;
+    spacing: 0px;
   }
 
   #element {
-      border:  0px;
-      padding: 0px 5px 0px 5px;
-      spacing: 0px;
+    border: 0px;
+    padding: 0px 5px 0px 5px;
+    spacing: 0px;
   }
 
   #element.normal.normal {
-      background-color: transparent;
+    background-color: transparent;
   }
 
   #element.normal.urgent {
-      background-color: @urgent-background;
+    background-color: @urgent-background;
   }
 
   #element.normal.active {
       background-color: @active-background;
-      text-color:       @background;
+      text-color: @background;
   }
 
   #element.selected.normal {
-      background-color: @selected-background;
-      text-color:       ${theme.foam};
+    background-color: @selected-background;
+    text-color: ${theme.foam};
   }
 
   #element.selected.urgent {
-      background-color: @selected-urgent-background;
+    background-color: @selected-urgent-background;
   }
 
   #element.selected.active {
-      background-color: @selected-active-background;
-      text-color:       @background;
+    background-color: @selected-active-background;
+    text-color: @background;
   }
 
   #element.alternate.normal {
-      background-color: transparent;
+    background-color: transparent;
   }
 
   #element.alternate.urgent {
-      background-color: @urgent-background;
+    background-color: @urgent-background;
   }
 
   #element.alternate.active {
-      background-color: @active-background;
+    background-color: @active-background;
   }
 ''
