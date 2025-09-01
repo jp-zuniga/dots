52146e54dddb36641b6c29d7d1bf8e6da6822134
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-01 14:18:46 -0600

    fix: fish config indentation

--- user/fish/fish-conf.nix
+++ user/fish/fish-conf.nix
@@ -6,13 +6,13 @@ pkgs.writeText "config.fish" ''
   fish_config theme choose rose-pine-moon
 
   # init starship for gui terminals
-  # if pgrep alacritty > /dev/null || pgrep code > /dev/null
-  #     starship init fish | source
+  # if pidof alacritty > /dev/null || pidof code > /dev/null
+  #   starship init fish | source
   # end
 
   # autostart hyprland on boot
   if ! pgrep Hyprland > /dev/null
-      hyprland > /dev/null
+    hyprland > /dev/null
   end
 
   ##############################
@@ -20,53 +20,53 @@ pkgs.writeText "config.fish" ''
   ##############################
 
   function gl --description "'git log' with custom default arguments"
-      git log --graph --pretty=format:'%C(magenta)%h%C(white) - %an - %C(yellow)%ar%C(auto) - %D%n%s'
+    git log --graph --pretty=format:'%C(magenta)%h%C(white) - %an - %C(yellow)%ar%C(auto) - %D%n%s'
   end
 
   function gsh --description "'git show' with custom default arguments"
-      git show --pretty=format:'%C(magenta)%h%C(white) - %an - %C(yellow)%ar%C(auto) - %D%n%s'
+    git show --pretty=format:'%C(magenta)%h%C(white) - %an - %C(yellow)%ar%C(auto) - %D%n%s'
   end
 
   function custom-eza --description "eza with custom defaults"
-      set -l dir ""
-      set -l flags "-"
-      set -l level 0
+    set -l dir ""
+    set -l flags "-"
+    set -l level 0
 
-      argparse 'a/all' 'l/long' 't/tree' 'r/recursive' -- $argv
-      or return
+    argparse 'a/all' 'l/long' 't/tree' 'r/recursive' -- $argv
+    or return
 
-      if set -q _flag_all
-          set flags "$flags"a
-      end
+    if set -q _flag_all
+      set flags "$flags"a
+    end
 
-      if set -q _flag_long
-          set flags "$flags"hl
-      else
-          set flags "$flags"x
-      end
+    if set -q _flag_long
+      set flags "$flags"hl
+    else
+      set flags "$flags"x
+    end
 
-      if set -q _flag_tree
-          set flags "$flags"T
-          set level 1
-      end
+    if set -q _flag_tree
+      set flags "$flags"T
+      set level 1
+    end
 
-      if set -q _flag_recursive
-          set flags "$flags"R
-          set level 2
-      end
+    if set -q _flag_recursive
+      set flags "$flags"R
+      set level 2
+    end
 
-      if set -q argv[1]
-          set dir (string replace --all '~' $HOME "$argv[1]")
-      end
+    if set -q argv[1]
+      set dir (string replace --all '~' $HOME "$argv[1]")
+    end
 
-      if test -z "$dir"
-          eza $flags --color=always --color-scale=all --icons=auto \
-                    --level=$level --sort=type --git-ignore \
-                    --group-directories-first
-      else
-          eza $flags --color=always --color-scale=all --icons=auto \
-                    --level=$level --sort=type --git-ignore \
-                    --group-directories-first "$dir"
-      end
+    if test -z "$dir"
+        eza $flags --color=always --color-scale=all --icons=auto \
+                   --level=$level --sort=type --git-ignore \
+                   --group-directories-first
+    else
+        eza $flags --color=always --color-scale=all --icons=auto \
+                   --level=$level --sort=type --git-ignore \
+                   --group-directories-first "$dir"
+    end
   end
 ''
