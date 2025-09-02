6d91d74eb3c5c2ba62a72a3016d9349d8f4abf47
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-01 18:05:31 -0600

    fix: bunch of firefox config bullshit
    chore: remove firefoxTheme input

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -1,23 +1,11 @@
 {
-  config,
-  inputs,
-  lib,
   pkgs,
   theme,
   ...
 }: {
   programs.firefox = {
     enable = true;
-    policies = import ./policies.nix {inherit lib pkgs theme;};
+    policies = import ./policies.nix {inherit pkgs theme;};
     preferences = import ./preferences.nix;
   };
-
-  system.activationScripts.firefoxSetup = ''
-    for PROFILE_DIR in ${config.users.users.jaq.home}/.mozilla/firefox/*.default; do
-      if [ -d "$PROFILE_DIR" ]; then
-        mkdir -p "$PROFILE_DIR/chrome"
-        cp -fr ${inputs.firefoxTheme}/dist/* "$PROFILE_DIR/chrome/"
-      fi
-    done
-  '';
 }
