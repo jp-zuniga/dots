fc0604bb14bf2fd5d469e94772fd642652d0effc
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-03 22:51:09 -0600

    fix: even more no good, terrible, bad, stupid, goofy bullshit

--- user/yazi/default.nix
+++ user/yazi/default.nix
@@ -5,7 +5,7 @@
   theme,
   ...
 }: let
-  yazTheme = import ./yazi-theme.nix {inherit pkgs theme;};
+  yaziTheme = import ./yazi-theme.nix {inherit pkgs theme;};
   yaziConf = import ./yazi-conf.nix {inherit pkgs theme;};
   yaziLocation = config.users.users.jaq.home + "/.config/yazi";
   flavorLocation = "${yaziLocation}/flavors/rose-pine-${theme.rosePineVariant}.yazi";
@@ -17,7 +17,7 @@ in {
   system.activationScripts.yaziSetup = ''
     mkdir -p ${yaziLocation} ${flavorLocation}
     ln -sf ${yaziConf} ${yaziLocation}/yazi.toml
-    ln -sf ${yazTheme} ${yaziLocation}/theme.toml
+    ln -sf ${yaziTheme} ${yaziLocation}/theme.toml
     cp -f ${inputs.yaziTheme}/* ${flavorLocation}
   '';
 }
