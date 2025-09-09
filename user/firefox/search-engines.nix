b143020e34c638a0273345aa5a251910ac8464ad
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-09 01:45:51 -0600

    feat: more firefox config

--- user/firefox/search-engines.nix
+++ user/firefox/search-engines.nix
@@ -3,10 +3,16 @@
 in {
   Add = [
     {
-      Alias = "sp";
+      Alias = "@sp";
       IconURL = "https://www.startpage.com/sp/cdn/favicons/favicon-gradient.ico";
       Name = "Startpage";
       URLTemplate = "https://www.startpage.com/sp/search?query={searchTerms}";
+    }
+    {
+      Alias = "@wiki";
+      IconURL = "https://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png";
+      Name = "Wikipedia";
+      URLTemplate = "https://wikipedia.org/wiki/Special:Search?search={searchTerms}";
     }
     {
       inherit icon;
@@ -22,16 +28,21 @@ in {
       Name = "nixopts";
       URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
     }
+    {
+      Alias = "@g";
+      Name = "Gooogle";
+      IconURL = "https://www.gstatic.com/marketing-cms/assets/images/d5/dc/cfe9ce8b4425b410b49b7f2dd3f3/g.webp=s48-fcrop64=1,00000000ffffffff-rw";
+      URLTemplate = "https://www.google.com/search?q={searchTerms}";
+    }
   ];
 
   Default = "Startpage";
   DefaultPrivate = "Startpage";
   PreventInstalls = true;
   Remove = [
-    "Actions"
     "Bing"
     "DuckDuckGo"
-    "History"
-    "Tabs"
+    "Google"
+    "Wikipedia (en)"
   ];
 }
