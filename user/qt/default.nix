95f42d455f90db4b5d7b1d9b91acbf017a3e6603
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-09 01:45:33 -0600

    fix: qt bullshit

--- user/qt/default.nix
+++ user/qt/default.nix
@@ -1,10 +1,40 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  theme,
+  ...
+}: let
+  kvantumConfig = pkgs.writeText "kvantum.kvconfig" ''
+    [General]
+    theme=rose-pine
+  '';
+
+  kvantumFlavor = "rose-pine-${theme.rosePineVariant}-iris";
+  kvantumLocation = "${config.users.users.jaq.home}/.config/Kvantum";
+in {
+  environment = {
+    sessionVariables = {
+      QT_QPA_PLATFORMTHEME = "qt5ct";
+    };
+
+    systemPackages = [
+      pkgs.libsForQt5.qt5ct
+      pkgs.libsForQt5.qtstyleplugin-kvantum
+      pkgs.rose-pine-kvantum
+    ];
+  };
+
+  system.activationScripts.kvantumSetup = {
+    deps = [];
+    text = ''
+      mkdir -p ${kvantumLocation} ${kvantumLocation}/rose-pine
+      ln -sf ${pkgs.rose-pine-kvantum}/share/Kvantum/themes/${kvantumFlavor}/${kvantumFlavor}.kvconfig ${kvantumLocation}/rose-pine
+      ln -sf ${kvantumConfig} ${kvantumLocation}/kvantum.kvconfig
+    '';
+  };
+
   qt = {
     enable = true;
     platformTheme = "qt5ct";
-    style = {
-      package = pkgs.rose-pine-kvantum;
-      name = "kvantum";
-    };
   };
 }
