9ec146be9715e06c0edc1f698b31f7ef740811af
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-20 16:50:12 -0600

    chore: add unstable channel to inputs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     yaziTheme = {
       url = "github:Mintass/rose-pine-moon.yazi";
