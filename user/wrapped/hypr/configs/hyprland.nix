057778a50548edaa530b4b6bd4eef516962ef076
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-22 18:41:13 -0600

    feat: mirror hyprland displays (i think)

--- user/wrapped/hypr/configs/hyprland.nix
+++ user/wrapped/hypr/configs/hyprland.nix
@@ -173,8 +173,8 @@ in {
   };
 
   monitor = [
-    "eDP-1, 1920x1080@60, 0x0, 1"
-    ", preferred, auto, 1"
+    "eDP-1, 1920x1080@60, 0x0, 1, mirror, DP-1"
+    ", preferred, auto, 1, mirror, eDP-1"
   ];
 
   windowrule = [
