f15df28f894e99bd0c20428f6e86a884499a9782
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-22 18:42:30 -0600

    feat: add copilot shit

--- user/vscode/default.nix
+++ user/vscode/default.nix
@@ -1,27 +1,38 @@
 {
-  pkgs,
   config,
+  inputs,
+  pkgs,
   ...
 }: let
+  unstable = import inputs.nixpkgs-unstable {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+  };
+
   vscodeConfLocation = config.users.users.jaq.home + "/.config/Code/User";
   vscodeConf = import ./vscode-conf.nix pkgs;
 in {
   environment.systemPackages = with pkgs; [
     (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        adpyke.codesnap
-        aaron-bond.better-comments
-        bbenoist.nix
-        charliermarsh.ruff
-        kamadorueda.alejandra
-        llvm-vs-code-extensions.vscode-clangd
-        ms-python.python
-        ms-python.vscode-pylance
-        mvllow.rose-pine
-        pkief.material-icon-theme
-        pkief.material-product-icons
-        tamasfe.even-better-toml
-      ];
+      vscodeExtensions = with vscode-extensions;
+        [
+          adpyke.codesnap
+          aaron-bond.better-comments
+          bbenoist.nix
+          charliermarsh.ruff
+          kamadorueda.alejandra
+          llvm-vs-code-extensions.vscode-clangd
+          ms-python.python
+          ms-python.vscode-pylance
+          mvllow.rose-pine
+          pkief.material-icon-theme
+          pkief.material-product-icons
+          tamasfe.even-better-toml
+        ]
+        ++ [
+          unstable.vscode-extensions.github.copilot
+          unstable.vscode-extensions.github.copilot-chat
+        ];
     })
   ];
 
