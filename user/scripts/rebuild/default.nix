339ae5ea5c00c057df576d11816815f913a05885
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-24 19:22:44 -0600

    fix: echo arguments

--- user/scripts/rebuild/default.nix
+++ user/scripts/rebuild/default.nix
@@ -21,7 +21,7 @@ pkgs.writeShellScriptBin "rebuild" ''
       echo "Exiting." && exit 0
     fi
 
-    echo -en "\nProceeding with rebuild..."
+    echo -e -n "\nProceeding with rebuild..."
   fi
 
   # autoformat
@@ -30,7 +30,7 @@ pkgs.writeShellScriptBin "rebuild" ''
   # show changes
   git diff "*.nix"
 
-  echo -en "\nRebuilding system..."
+  echo -e -n "\nRebuilding system..."
 
   echo && sudo nixos-rebuild switch --flake .#$HOST &> $S_LOG || (\
     cat $S_LOG | grep --color error && \
