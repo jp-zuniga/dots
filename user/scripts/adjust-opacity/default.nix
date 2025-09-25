c685e167026cb3ab68327912b6f6bede44cae5bd
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-24 19:17:29 -0600

    fix: adjust-opacity script

--- user/scripts/adjust-opacity/default.nix
+++ user/scripts/adjust-opacity/default.nix
@@ -1,26 +1,21 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "adjust-opacity" ''
-  WINDOW=$(hyprctl activewindow -j | jq -r '.address')
-  OLD_ALPHA=$(hyprctl activewindow -j | jq -r '.alpha')
-
-  if [ "$OLD_ALPHA" = "null" ] || [ -z "$OLD_ALPHA" ]; then
-    OLD_ALPHA=1.0
-  fi
+  OLD_ALPHA=$(hyprctl getprop activewindow alpha)
 
   case "$1" in
-    "increase")
+    "-i")
       NEW_ALPHA=$(echo "$OLD_ALPHA + 0.1" | bc -l)
       if (( $(echo "$NEW_ALPHA > 1.0" | bc -l) )); then
         NEW_ALPHA=1.0
       fi
-      ;;
-    "decrease")
+    ;;
+    "-d")
       NEW_ALPHA=$(echo "$OLD_ALPHA - 0.1" | bc -l)
       if (( $(echo "$NEW_ALPHA < 0.25" | bc -l) )); then
         NEW_ALPHA=0.25
       fi
-      ;;
+    ;;
   esac
 
-  hyprctl dispatch setprop address:$WINDOW alpha $NEW_ALPHA
+  hyprctl dispatch setprop activewindow alpha $NEW_ALPHA
 ''
