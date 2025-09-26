871f28cf173f17284e0a44b3a48e2ca8489f8d9e
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-26 01:49:21 -0600

    fix: more imports

--- apps/pkgs/default.nix
+++ apps/pkgs/default.nix
@@ -2,14 +2,14 @@
   inputs,
   lib,
   pkgs,
+  theme,
   ...
 }: let
-  theme = import ..//theme pkgs;
   unstable = import inputs.nixpkgs-unstable {system = pkgs.stdenv.hostPlatform.system;};
 
-  scripts = pkgs.callPackage ./scripts {inherit pkgs theme;};
-  wrapped = pkgs.callPackage ./wrapped {inherit lib pkgs unstable theme;};
+  scripts = import ./scripts {inherit pkgs theme;};
+  wrapped = import ./wrapped {inherit lib pkgs unstable theme;};
 in {
   environment.systemPackages = builtins.attrValues (scripts // wrapped);
-  imports = [./wrapped/btop-theme.nix];
+  imports = [./wrapped/btop/btop-theme.nix];
 }
