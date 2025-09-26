dca19f05d6ce341ea6c97708c98572f5286fecb3
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-26 01:12:25 -0600

    fix: duplicate base16 colors

--- theme/default.nix
+++ theme/default.nix
@@ -1,57 +1,21 @@
-{pkgs, ...}: let
-  base00 = "#232136";
-  base01 = "#2a273f";
-  base02 = "#393552";
-  base03 = "#6e6a86";
-  base04 = "#908caa";
-  base05 = "#e0def4";
-  base06 = "#eb6f92";
-  base07 = "#f6c177";
-  base08 = "#ea9a97";
-  base09 = "#3e8fb0";
-  base0A = "#9ccfd8";
-  base0B = "#c4a7e7";
-  base0C = "#f6c177";
-  base0D = "#2a283e";
-  base0E = "#44415a";
-  base0F = "#56526e";
-in {
-  inherit
-    base00
-    base01
-    base02
-    base03
-    base04
-    base05
-    base06
-    base07
-    base08
-    base09
-    base0A
-    base0B
-    base0C
-    base0D
-    base0E
-    base0F
-    ;
-
-  bg = base00;
-  surface = base01;
-  overlay = base02;
-  muted = base03;
-  subtle = base04;
-  text = base05;
-  love = base06;
-  gold = base07;
-  rose = base08;
-  pine = base09;
-  foam = base0A;
-  iris = base0B;
-  highlightLow = base0D;
-  highlightMed = base0E;
-  highlightHigh = base0F;
-
+{pkgs, ...}: {
   rosePineVariant = "moon";
+
+  bg = "#232136";
+  surface = "#2a273f";
+  overlay = "#393552";
+  muted = "#6e6a86";
+  subtle = "#908caa";
+  text = "#e0def4";
+  love = "#eb6f92";
+  rose = "#ea9a97";
+  gold = "#f6c177";
+  foam = "#9ccfd8";
+  pine = "#3e8fb0";
+  iris = "#c4a7e7";
+  highlightLow = "#2a283e";
+  highlightMed = "#44415a";
+  highlightHigh = "#56526e";
 
   cursor = {
     hypr = {
