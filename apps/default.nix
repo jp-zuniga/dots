3b9c13c00cee53ca93d797d3cba8174e530f1ca7
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-26 12:54:00 -0600

    fix: re-add stuff i forgot about

--- apps/default.nix
+++ apps/default.nix
@@ -9,20 +9,53 @@
     ./programs
   ];
 
-  environment.systemPackages = [
-    pkgs.alejandra
-    pkgs.bat-extras.batdiff
-    pkgs.bat-extras.batgrep
-    pkgs.bat-extras.batman
-    pkgs.discord
-    pkgs.dust
-    pkgs.gimp3
-    pkgs.jetbrains.idea-ultimate
-    pkgs.libreoffice-fresh
-    pkgs.microfetch
-    pkgs.mpv
-    pkgs.pastel
-    pkgs.qview
-    pkgs.uv
-  ];
+  environment = {
+    extraSetup = ''
+      set -eu
+
+      for file in \
+        Alacritty.desktop base.desktop btop.desktop code-url-handler.desktop \
+        cups.desktop draw.desktop fish.desktop math.desktop mpv.desktop \
+        nixos-manual.desktop startcenter.desktop yazi.desktop
+      do
+        rm -f "$out/share/applications/$file" || true
+      done
+
+      rename() {
+        [ -f "$1" ] && sed -i "s/$2/$3/" "$1" || true
+      }
+
+      rename "$out/share/applications/gimp.desktop" "GNU Image Manipulation Program" "GIMP"
+      rename "$out/share/applications/idea-ultimate.desktop" "IntelliJ IDEA" "IDEA"
+      rename "$out/share/applications/calc.desktop" "LibreOffice Calc" "Calc"
+      rename "$out/share/applications/impress.desktop" "LibreOffice Impress" "Impress"
+      rename "$out/share/applications/writer.desktop" "LibreOffice Writer" "Writer"
+      rename "$out/share/applications/virt-manager.desktop" "Virtual Machine Manager" "VM Manager"
+      rename "$out/share/applications/code.desktop" "Visual Studio Code" "Code"
+    '';
+
+    systemPackages = [
+      pkgs.alejandra
+      pkgs.bat-extras.batdiff
+      pkgs.bat-extras.batgrep
+      pkgs.bat-extras.batman
+      pkgs.discord
+      pkgs.dust
+      pkgs.gimp3
+      pkgs.jetbrains.idea-ultimate
+      pkgs.libreoffice-fresh
+      pkgs.microfetch
+      pkgs.mpv
+      pkgs.pastel
+      pkgs.qview
+      pkgs.uv
+    ];
+  };
+
+  programs = {
+    java = {
+      enable = true;
+      package = pkgs.jdk17;
+    };
+  };
 }
