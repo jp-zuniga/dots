acae8661168e3702bf9a1aaa4de7b88b648de68f
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-26 01:53:03 -0600

    refactor: split systemPackages

--- apps/default.nix
+++ apps/default.nix
@@ -14,25 +14,14 @@
     pkgs.bat-extras.batdiff
     pkgs.bat-extras.batgrep
     pkgs.bat-extras.batman
-    pkgs.bc
-    pkgs.brightnessctl
-    pkgs.brillo
-    pkgs.clang-tools
-    pkgs.delta
     pkgs.discord
     pkgs.dust
     pkgs.gimp3
-    pkgs.hyprpicker
-    pkgs.hyprshot
-    pkgs.hyprsunset
     pkgs.jetbrains.idea-ultimate
-    pkgs.jq
-    pkgs.libnotify
     pkgs.libreoffice-fresh
     pkgs.microfetch
     pkgs.mpv
     pkgs.pastel
-    pkgs.swww
     pkgs.qview
     pkgs.uv
   ];
--- apps/programs/default.nix
+++ apps/programs/default.nix
@@ -12,4 +12,17 @@
     ./vscode
     ./yazi
   ];
+
+  environment.systemPackages = [
+    pkgs.bc
+    pkgs.brightnessctl
+    pkgs.brillo
+    pkgs.delta
+    pkgs.hyprpicker
+    pkgs.hyprshot
+    pkgs.hyprsunset
+    pkgs.jq
+    pkgs.libnotify
+    pkgs.swww
+  ];
 }
