6940654e05b5772dff8a43d6444582bc67d27e78
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-09-27 06:58:45 -0600

    feat: improve vm performance?

--- system/virtualisation/default.nix
+++ system/virtualisation/default.nix
@@ -1,8 +1,27 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  boot.kernelModules = ["vfio-pci"];
+  environment.systemPackages = [pkgs.virtio-win];
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = builtins.attrNames config.users.users;
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        ovmf = {
+          enable = true;
+          packages = [pkgs.OVMFFull.fd];
+        };
+
+        swtpm.enable = true;
+      };
+    };
+
     spiceUSBRedirection.enable = true;
   };
+
+  services.spice-vdagentd.enable = true;
 }
