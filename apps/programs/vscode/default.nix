3aeb2dfda24494fd06027ec75ce5c21d12181c65
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-02 20:10:27 -0600

    fix: remove stuffffffff

--- apps/programs/vscode/default.nix
+++ apps/programs/vscode/default.nix
@@ -4,18 +4,12 @@
   pkgs,
   ...
 }: let
-  unstable = import inputs.nixpkgs-unstable {
-    system = pkgs.stdenv.hostPlatform.system;
-    config.allowUnfree = true;
-  };
-
   vscodeConfLocation = config.users.users.jaq.home + "/.config/Code/User";
   vscodeConf = import ./vscode-conf.nix pkgs;
 in {
   environment.systemPackages = with pkgs; [
     (vscode-with-extensions.override {
       vscodeExtensions = [
-        vscode-extensions.adpyke.codesnap
         vscode-extensions.aaron-bond.better-comments
         vscode-extensions.bbenoist.nix
         vscode-extensions.charliermarsh.ruff
@@ -28,8 +22,6 @@ in {
         vscode-extensions.pkief.material-icon-theme
         vscode-extensions.pkief.material-product-icons
         vscode-extensions.tamasfe.even-better-toml
-        unstable.vscode-extensions.github.copilot
-        unstable.vscode-extensions.github.copilot-chat
       ];
     })
   ];
