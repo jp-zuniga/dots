54a4f5e5a20c72ee8b02151fedb480b83a5300b6
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-04 13:55:49 -0600

    fix: mouse thing

--- apps/programs/packettracer/default.nix
+++ apps/programs/packettracer/default.nix
@@ -1,27 +1,25 @@
 {
+  config,
   lib,
   pkgs,
   theme,
   ...
-}: {
-  environment.systemPackages = [
-    (pkgs.ciscoPacketTracer8.override {
-      packetTracerSource = /home/jaq/docs/CiscoPacketTracer822_amd64_signed.deb;
-    })
-  ];
-
+}: let
+  cpt8 = pkgs.ciscoPacketTracer8.override {
+    packetTracerSource = /home/jaq/docs/CiscoPacketTracer822_amd64_signed.deb;
+  };
+in {
+  environment.systemPackages = [cpt8];
   programs.firejail = {
     enable = true;
-    wrappedBinaries = {
-      packettracer8 = {
-        executable = lib.getExe pkgs.ciscoPacketTracer8;
-        extraArgs = [
-          "--net=none"
-          "--noprofile"
-          "--env=QT_QPA_PLATFORMTHEME="
-          "--env=QT_STYLE_OVERRIDE=Fusion"
-        ];
-      };
+    wrappedBinaries.packettracer8 = {
+      executable = lib.getExe cpt8;
+      extraArgs = [
+        "--net=none"
+        "--noprofile"
+        "--env=QT_QPA_PLATFORMTHEME="
+        "--env=QT_STYLE_OVERRIDE=Fusion"
+      ];
 
       desktop = "${pkgs.ciscoPacketTracer8}/share/applications/cisco-pt8.desktop.desktop";
     };
