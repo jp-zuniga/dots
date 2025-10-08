92e3d29d671012a1c9c46d7130b1db75add9837e
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-08 11:36:15 -0600

    fix: switch to bancadesk postgres config

--- system/services/default.nix
+++ system/services/default.nix
@@ -1,14 +1,23 @@
-{...}: {
-  services = {
-    postgresql = {
-      enable = true;
-      ensureDatabases = ["nutriplan"];
-      authentication = ''
-        #type database  DBuser  auth-method
-        local all       all     trust
-      '';
-    };
+{
+  config,
+  pkgs,
+  ...
+}: {
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = ["bancadesk"];
+    ensureUsers = [{name = "jaq";}];
 
-    printing.enable = false;
+    # username must match pg user for peer authentication
+    authentication = ''
+      local   all     all                   peer
+      host    all     all     127.0.0.1/32  scram-sha-256
+      host    all     all     ::1/128       scram-sha-256
+    '';
+
+    initialScript = pkgs.writeText "init-bancadesk.sql" ''
+      ALTER DATABASE bancadesk OWNER TO jaq;
+      GRANT ALL PRIVILEGES ON DATABASE bancadesk TO jaq;
+    '';
   };
 }
