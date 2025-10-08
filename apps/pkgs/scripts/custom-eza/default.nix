2309bcf8c182c1b07ea0a0ddeea51c31f2adea4e
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-10-08 11:33:08 -0600

    fix: custom-eza arg parsing

--- apps/pkgs/scripts/custom-eza/default.nix
+++ apps/pkgs/scripts/custom-eza/default.nix
@@ -2,51 +2,84 @@
 pkgs.writeShellScriptBin "custom-eza" ''
   set -euo pipefail
 
+  declare -a args
+
   dir=""
-  flags="-"
   level=0
   recursive_count=0
 
+  # parse args
   while [[ $# -gt 0 ]]; do
     case "$1" in
-      -a | --all)
-        flags="''${flags}a"
+      --)
+        # end of options
+        shift
+        break
         ;;
-      -l | --long)
-        flags="''${flags}hl"
+      --all)
+        args+=("--all")
         ;;
-      -t | --tree)
-        flags="''${flags}T"
+      --long)
+        args+=("-l" "-h")
+        ;;
+      --tree)
+        args+=("--tree")
         level=1
         ;;
-      -r | --recursive)
-        flags="''${flags}R"
+      --recursive)
+        args+=("-R")
         recursive_count=$((recursive_count + 1))
         ;;
+
+      # clustered short options, e.g. -atrrr
+      -[!-]*)
+        cluster="''${1#-}"
+        for (( i=0; i<''${#cluster}; i++ )); do
+          c="''${cluster:i:1}"
+          case "$c" in
+            a) args+=("-a") ;;
+            l) args+=("-l" "-h") ;;
+            t) args+=("--tree"); level=1 ;;
+            r) args+=("-R"); recursive_count=$((recursive_count + 1)) ;;
+            *)
+              echo "Unknown option: -$c" >&2
+              exit 1
+              ;;
+          esac
+        done
+        ;;
+      -*)
+        echo "Unknown flag: $1" >&2
+        exit 1
+        ;;
       *)
-          if [[ -z "$dir" ]]; then
-            # expandir ~
-            dir="''${1/#\~/$HOME}"
-          else
-            echo "Unkown argument: $1" >&2
-            exit 1
-          fi
-          ;;
-      esac
+        if [[ -z "$dir" ]]; then
+          dir="''${1/#\~/$HOME}"
+        else
+          echo "Unknown argument: $1" >&2
+          exit 1
+        fi
+        ;;
+    esac
     shift
   done
 
-  if [[ $recursive_count -gt 0 ]]; then
+  # accept first positional after `--` as dir
+  if [[ $# -gt 0 && -z "$dir" ]]; then
+    dir="''${1/#\~/$HOME}"
+  fi
+
+  if (( recursive_count > 0 )); then
     level=$recursive_count
   fi
 
+  common=(--color=always --color-scale=all --icons=auto
+          --level="''${level}" --sort=type --git-ignore
+          --group-directories-first)
+
   if [[ -z "$dir" ]]; then
-    eza $flags --color=always --color-scale=all --icons=auto \
-               --level=$level --sort=type --git-ignore \
-               --group-directories-first
+    eza "''${args[@]}" "''${common[@]}"
   else
-    eza $flags --color=always --color-scale=all --icons=auto \
-               --level=$level --sort=type --git-ignore \
-               --group-directories-first "$dir"
+    eza "''${args[@]}" "''${common[@]}" -- "''${dir}"
   fi
 ''
