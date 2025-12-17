df3d98f00d9b58b7cc432552781eb4a79c0c4741
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-17 15:30:25 -0600

    fix: don't assume commands are always present

--- apps/pkgs/scripts/adjust-opacity/default.nix
+++ apps/pkgs/scripts/adjust-opacity/default.nix
@@ -1,21 +1,23 @@
-{pkgs, ...}:
-pkgs.writeShellScriptBin "adjust-opacity" ''
-  OLD_ALPHA=$(hyprctl getprop activewindow alpha)
+{pkgs, ...}: let
+  bc-bin = "${pkgs.bc}/bin/bc";
+in
+  pkgs.writeShellScriptBin "adjust-opacity" ''
+    OLD_ALPHA=$(hyprctl getprop activewindow alpha)
 
-  case "$1" in
-    "-i")
-      NEW_ALPHA=$(echo "$OLD_ALPHA + 0.1" | bc -l)
-      if (( $(echo "$NEW_ALPHA > 1.0" | bc -l) )); then
-        NEW_ALPHA=1.0
-      fi
-    ;;
-    "-d")
-      NEW_ALPHA=$(echo "$OLD_ALPHA - 0.1" | bc -l)
-      if (( $(echo "$NEW_ALPHA < 0.25" | bc -l) )); then
-        NEW_ALPHA=0.25
-      fi
-    ;;
-  esac
+    case "$1" in
+      "-i")
+        NEW_ALPHA=$(echo "$OLD_ALPHA + 0.1" | ${bc-bin} -l)
+        if (( $(echo "$NEW_ALPHA > 1.0" | ${bc-bin} -l) )); then
+          NEW_ALPHA=1.0
+        fi
+      ;;
+      "-d")
+        NEW_ALPHA=$(echo "$OLD_ALPHA - 0.1" | ${bc-bin} -l)
+        if (( $(echo "$NEW_ALPHA < 0.25" | ${bc-bin} -l) )); then
+          NEW_ALPHA=0.25
+        fi
+      ;;
+    esac
 
-  hyprctl dispatch setprop activewindow alpha $NEW_ALPHA
-''
+    hyprctl dispatch setprop activewindow alpha $NEW_ALPHA
+  ''
