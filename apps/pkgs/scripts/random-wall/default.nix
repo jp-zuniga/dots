2d7315a8587aef64d13a60c52b8926502bcab044
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-17 15:30:25 -0600

    fix: don't assume commands are always present

--- apps/pkgs/scripts/random-wall/default.nix
+++ apps/pkgs/scripts/random-wall/default.nix
@@ -1,25 +1,27 @@
-{pkgs, ...}:
-pkgs.writeShellScriptBin "random-wall" ''
-  mkdir -p ~/.cache
+{pkgs, ...}: let
+  swww-bin = "${pkgs.swww}/bin/swww";
+in
+  pkgs.writeShellScriptBin "random-wall" ''
+    mkdir -p ~/.cache
 
-  WALL_DIR=$HOME/wallpapers
+    WALL_DIR=$HOME/wallpapers
 
-  # select a random subdirectory (excluding .git)
-  RANDOM_FLAVOR=$(\
-    find "$WALL_DIR" -type d -name ".git" -prune -o -type d -print | \
-    shuf -n 1 \
-  )
+    # select a random subdirectory (excluding .git)
+    RANDOM_FLAVOR=$(\
+      find "$WALL_DIR" -type d -name ".git" -prune -o -type d -print | \
+      shuf -n 1 \
+    )
 
-  # select a random file within that subdirectory (excluding .git)
-  RANDOM_WALL=$(\
-    find "$RANDOM_FLAVOR" -path "*/.git" -prune -o -type f -print | \
-    shuf -n 1 \
-  )
+    # select a random file within that subdirectory (excluding .git)
+    RANDOM_WALL=$(\
+      find "$RANDOM_FLAVOR" -path "*/.git" -prune -o -type f -print | \
+      shuf -n 1 \
+    )
 
-  ln -sf "$RANDOM_WALL" ~/.cache/.current-wall
+    ln -sf "$RANDOM_WALL" ~/.cache/.current-wall
 
-  swww img "$RANDOM_WALL" \
-    --transition-type any \
-    --transition-duration 0.5 \
-    --transition-fps 120
-''
+    ${swww-bin} img "$RANDOM_WALL" \
+      --transition-type any \
+      --transition-duration 0.5 \
+      --transition-fps 120
+  ''
