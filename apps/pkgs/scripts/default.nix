136ff81e9280a65cb9cc43c7951bdb8aeb0ec185
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-17 15:33:19 -0600

    refactor: rm dumbass subdirectories

--- apps/pkgs/scripts/default.nix
+++ apps/pkgs/scripts/default.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
-  adjust-opacity = pkgs.callPackage ./adjust-opacity pkgs;
-  custom-eza = pkgs.callPackage ./custom-eza pkgs;
-  focus = pkgs.callPackage ./focus pkgs;
-  random-wall = pkgs.callPackage ./random-wall pkgs;
-  rebuild = pkgs.callPackage ./rebuild pkgs;
-  upgrade = pkgs.callPackage ./upgrade pkgs;
+  adjust-opacity = pkgs.callPackage ./adjust-opacity.nix pkgs;
+  custom-eza = pkgs.callPackage ./custom-eza.nix pkgs;
+  focus = pkgs.callPackage ./focus.nix pkgs;
+  random-wall = pkgs.callPackage ./random-wall.nix pkgs;
+  rebuild = pkgs.callPackage ./rebuild.nix pkgs;
+  upgrade = pkgs.callPackage ./upgrade.nix pkgs;
 }
