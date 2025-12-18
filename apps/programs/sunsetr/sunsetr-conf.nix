d0a8a823534922173f8569790ec6940d43f0a74f
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-17 21:08:06 -0600

    refactor: don't wrap `sunsetr`
    fix: also make sure it WORKS

--- apps/programs/sunsetr/sunsetr-conf.nix
+++ apps/programs/sunsetr/sunsetr-conf.nix
@@ -1,18 +1,20 @@
 {pkgs, ...}:
 pkgs.writeText "sunsetr.toml" ''
-  backend = "hyprland"
-  smoothing = true
-  transition_mode = "geo"
+  backend = "wayland"
 
+  sunset = "18:00:00"
+  sunrise = "06:00:00"
+  transition_duration = 5
+  transition_mode = "center"
+
+  smoothing = true
   startup_duration = 3
   shutdown_duration = 3
-
   adaptive_interval = 1
-  update_interval = 60
 
   day_gamma = 100
   day_temp = 6500
 
   night_gamma = 90
-  night_temp = 3250
+  night_temp = 3750
 ''
