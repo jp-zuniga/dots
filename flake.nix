60aeb2dc0cfcfc29027964902e48154567e57e93
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-18 16:26:30 -0600

    feat: `flake-parts` has arrived

--- flake.nix
+++ flake.nix
@@ -2,17 +2,20 @@
   description = "rawdogging nix for shits and giggles";
 
   inputs = {
-    homix = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:sioodmy/homix";
-    };
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    spicetify-nix = {
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    homix = {
+      url = "github:sioodmy/homix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     yaziTheme = {
@@ -21,12 +24,12 @@
     };
   };
 
-  outputs = inputs @ {self, ...}: {
-    nixosConfigurations = import ./hosts inputs;
-    nixosModules =
-      import ./modules
-      // {
-        system = import ./system;
-      };
-  };
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [
+        ./theme/flake-module.nix
+        ./hosts/t14s/flake-module.nix
+      ];
+    };
 }
