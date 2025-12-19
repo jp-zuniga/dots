f418f18ffc637253d9828fc48bdff538683e8e65
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-18 22:22:07 -0600

    fix: `capitalize.nix`
    should've been returning the function, not a set with the function

--- modules/lib/capitalize.nix
+++ modules/lib/capitalize.nix
@@ -1,7 +1,5 @@
-{lib, ...}: {
-  capitalize = str: let
-    first = lib.substring 0 1 str;
-    rest = lib.substring 1 (lib.stringLength str - 1) str;
-  in
-    lib.toUpper first + rest;
-}
+{lib, ...}: str: let
+  first = lib.substring 0 1 str;
+  rest = lib.substring 1 (lib.stringLength str - 1) str;
+in
+  lib.toUpper first + rest
