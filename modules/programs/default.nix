92e956b0ccc4b0286b5e06dd16475522ae50becc
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-18 21:51:42 -0600

    breaking: i forgot more imports!

---
+++ modules/programs/default.nix
@@ -0,0 +1,22 @@
+{
+  imports = [
+    ./eza
+    ./firefox
+    ./fish
+    ./git
+    ./gtk
+    ./jetbrains
+    ./starship
+    ./sunsetr
+    ./vscode
+    ./yazi
+
+    ./cursor.nix
+    ./packages.nix
+    ./prismlauncher.nix
+    ./qbittorrent.nix
+    ./qt.nix
+    ./spicetify.nix
+    ./steam.nix
+  ];
+}
