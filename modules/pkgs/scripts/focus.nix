6b0af88124af24c1b490a21c5dc2bd6a1d413d0a
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-19 14:20:13 -0600

    fix: get actual `waybar` binary, not store path

--- modules/pkgs/scripts/focus.nix
+++ modules/pkgs/scripts/focus.nix
@@ -4,7 +4,9 @@
   ...
 }: let
   hctl = "${pkgs.hyprland}/bin/hyprctl";
-  waybar = pkgs.callPackage ../wrapped/waybar {inherit theme;};
+
+  wyb-pkg = pkgs.callPackage ../wrapped/waybar {inherit theme;};
+  wyb = "${wyb-pkg}/bin/waybar";
 in
   pkgs.writeShellScriptBin "focus" ''
     MODE=$(${hctl} getoption animations:enabled | awk 'NR==1{print $2}')
@@ -12,27 +14,27 @@ in
     # disable visual eye-candy
     if [ "$MODE" = 1 ] ; then
       ! pidof waybar || pkill waybar
-      ${hctl} keyword animations:enabled 0 &
-      ${hctl} keyword decoration:blur:enabled 0 &
-      ${hctl} keyword decoration:inactive_opacity 1.0 &
-      ${hctl} keyword decoration:rounding 0 &
-      ${hctl} keyword decoration:shadow:enabled 0 &
-      ${hctl} keyword general:gaps_in 0 &
-      ${hctl} keyword general:gaps_out 0 &
-      ${hctl} keyword general:border_size 1 &
+      ${hctl} keyword animations:enabled 0
+      ${hctl} keyword decoration:blur:enabled 0
+      ${hctl} keyword decoration:inactive_opacity 1.0
+      ${hctl} keyword decoration:rounding 0
+      ${hctl} keyword decoration:shadow:enabled 0
+      ${hctl} keyword general:gaps_in 0
+      ${hctl} keyword general:gaps_out 0
+      ${hctl} keyword general:border_size 1
       exit 0
 
     # re-enable eye-candy
     else
-      ${hctl} keyword animations:enabled 1 &
-      ${hctl} keyword decoration:blur:enabled 1 &
-      ${hctl} keyword decoration:inactive_opacity 0.8 &
-      ${hctl} keyword decoration:rounding 5 &
-      ${hctl} keyword decoration:shadow:enabled 0 &
-      ${hctl} keyword general:gaps_in 5 &
-      ${hctl} keyword general:gaps_out 5 &
-      ${hctl} keyword general:border_size 3 &
-      pidof waybar || ${waybar}
+      ${hctl} keyword animations:enabled 1
+      ${hctl} keyword decoration:blur:enabled 1
+      ${hctl} keyword decoration:inactive_opacity 0.8
+      ${hctl} keyword decoration:rounding 5
+      ${hctl} keyword decoration:shadow:enabled 0
+      ${hctl} keyword general:gaps_in 5
+      ${hctl} keyword general:gaps_out 5
+      ${hctl} keyword general:border_size 3
+      pidof waybar || ${wyb}
       exit 0
     fi
 
