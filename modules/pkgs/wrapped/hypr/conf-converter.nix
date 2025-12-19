49678f2764db0b0913d56d774a090b7af21ef30b
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-19 14:53:50 -0600

    fix: license notices and keybind changes

--- modules/pkgs/wrapped/hypr/conf-converter.nix
+++ modules/pkgs/wrapped/hypr/conf-converter.nix
@@ -1,9 +1,9 @@
-# originally written by: sioodmy
-#   - author: https://github.com/sioodmy
-#   - source: https://github.com/sioodmy/dotfiles/blob/15ff23fc19cff8a5e4903bf95e93be025520c6b5/user/wrapped/hypr/tohyprconf.nix
+# - author: https://github.com/sioodmy
+# - source: https://github.com/sioodmy/dotfiles/blob/15ff23fc19cff8a5e4903bf95e93be025520c6b5/user/wrapped/hypr/tohyprconf.nix
 #
-# licensed under: GPLv3
-#   - original license: https://github.com/sioodmy/dotfiles/blob/15ff23fc19cff8a5e4903bf95e93be025520c6b5/LICENSE
+# license:
+#   - GPLv3
+#     - https://github.com/sioodmy/dotfiles/blob/15ff23fc19cff8a5e4903bf95e93be025520c6b5/LICENSE
 #
 # changes:
 #   - renamed function to `confConverter`
