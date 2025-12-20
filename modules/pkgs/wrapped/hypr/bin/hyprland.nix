295ba775fc82a08a9162fa98a50e5f4498f98485
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-20 11:28:00 -0600

    fix: kill extra `Hyprland` binary

--- modules/pkgs/wrapped/hypr/bin/hyprland.nix
+++ modules/pkgs/wrapped/hypr/bin/hyprland.nix
@@ -20,7 +20,9 @@
 
     buildInputs = [pkgs.makeWrapper];
     postBuild = ''
-      wrapProgram $out/bin/hyprland --add-flags "--config ${conf}"
+      rm -f $out/bin/Hyprland $out/bin/hyprland
+
+      makeWrapper ${hyprland}/bin/Hyprland $out/bin/hyprland --add-flags "--config ${conf}"
     '';
   };
 in
