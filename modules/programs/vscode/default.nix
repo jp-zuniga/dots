9e84797fe4208ce2123b847fec5ccf174b578f96
jp-zuniga <jp.zuniga.dev@gmail.com>
2026-02-16 19:26:30 -0600

    fix: update vscode config
    meta: switch to fhs version for `ty` extension

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -4,25 +4,30 @@
   ...
 }: {
   environment.systemPackages = [
-    (pkgs.vscode-with-extensions.override {
-      vscodeExtensions = let
-        vsc-pkgs = pkgs.vscode-extensions;
-      in [
-        vsc-pkgs.bbenoist.nix
-        vsc-pkgs.bierner.markdown-preview-github-styles
-        vsc-pkgs.charliermarsh.ruff
-        vsc-pkgs.editorconfig.editorconfig
-        vsc-pkgs.kamadorueda.alejandra
-        vsc-pkgs.llvm-vs-code-extensions.vscode-clangd
-        vsc-pkgs.ms-python.python
-        vsc-pkgs.ms-python.vscode-pylance
-        vsc-pkgs.mvllow.rose-pine
-        vsc-pkgs.pkief.material-icon-theme
-        vsc-pkgs.pkief.material-product-icons
-        vsc-pkgs.rust-lang.rust-analyzer
-        vsc-pkgs.tamasfe.even-better-toml
-      ];
-    })
+    pkgs.vscode-fhs
+
+    # (pkgs.vscode-with-extensions.override {
+    #   vscodeExtensions = let
+    #     vsc-pkgs = pkgs.vscode-extensions;
+    #   in [
+    #     vsc-pkgs.astral-sh.ty
+    #     vsc-pkgs.bbenoist.nix
+    #     vsc-pkgs.bierner.markdown-preview-github-styles
+    #     vsc-pkgs.charliermarsh.ruff
+    #     vsc-pkgs.editorconfig.editorconfig
+    #     vsc-pkgs.esbenp.prettier-vscode
+    #     vsc-pkgs.kamadorueda.alejandra
+    #     vsc-pkgs.llvm-vs-code-extensions.vscode-clangd
+    #     vsc-pkgs.ms-python.python
+    #     vsc-pkgs.ms-python.vscode-pylance
+    #     vsc-pkgs.mvllow.rose-pine
+    #     vsc-pkgs.pkief.material-icon-theme
+    #     vsc-pkgs.pkief.material-product-icons
+    #     vsc-pkgs.rust-lang.rust-analyzer
+    #     vsc-pkgs.tamasfe.even-better-toml
+    #     vsc-pkgs.usernamehw.errorlens
+    #   ];
+    # })
   ];
 
   system.activationScripts.vscodeSetup = {
